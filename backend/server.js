0bd3568b0d7e6a4ad3746963a5d7101042e32adf
Amankumar-007 <amanr3388@gmail.com>
2025-04-29 23:57:22 +0530

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
-const http = require('http');
-const app = require('./index.js');
+const http = require("http");
+const app = require("./index.js");
+const connectDB = require("./db/db.js");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+connectDB();
+
+
+
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}
-);
+  console.log(`Server is running on port ${port}`);
+});
